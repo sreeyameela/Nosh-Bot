3a4e3876a79521cfc7b6f64249cd2852de540c51
Sreeya Meela <sreeyameela@Sreeyas-MacBook-Air.local>
2023-04-14 19:53:38 -0400

    bot

--- src/App.js
+++ src/App.js
@@ -1,25 +1,181 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import ChatBot from 'react-simple-chatbot';
+import { ThemeProvider } from 'styled-components';
+import headerAvatar from './pic/nosh.png'
+import botAvatar from './pic/nosh.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const theme = {
+  background: 'white',
+  headerBgColor: '#ed872d',
+  headerFontSize: '20px',
+  botBubbleColor: '#F5F5F5',
+  headerFontColor: 'white',
+  botFontColor: 'black',
+  userBubbleColor: '#ed872d',
+  userFontColor: 'white',
+  headerImage: 'none', 
+};
+const steps = [
+  {
+    id: '0',
+    message: 'Hello! What is your name?',
+    trigger: '1',
+  },
+  {
+    id: '1',
+    user: true,
+    metadata: {
+      name: 'name',
+      value: '{{{raw}}}'
+    },
+    trigger: '2',
+  },
+  {
+    id: '2',
+    message: 'Nice to meet you, {previousValue}! Welcome to our restaurant. What type of food would you like to order today?',
+    trigger: '3',
+  },
+  {
+    id: '3',
+    options: [
+      { value: 'burger', label: 'Burger', trigger: '4' },
+      { value: 'pizza', label: 'Pizza', trigger: '4' },
+      { value: 'pasta', label: 'Pasta', trigger: '4' },
+      { value: 'drink', label: 'Drink', trigger: '5' },
+    ],
+  },
+  {
+    id: '4',
+    message: 'How many would you like to order?',
+    trigger: 'food',
+  },
+  {
+    id: '5',
+    message: 'Would you like to order a drink?',
+    trigger: '7',
+  },
+  {
+    id: 'food',
+    user: true,
+   
+    validator: (value) => {
+      if (isNaN(value)) {
+        return 'Please enter quantity';
+      } else if (value < 1) {
+        return 'Please enter a valid quantity';
+      } else {
+        return true;
+      }
+    },
+    trigger: '5',
+  },
+  {
+    id: '7',
+    options: [
+      { value: 'water', label: 'Water', trigger: '6' },
+      { value: 'soda', label: 'Soda', trigger: '6' },
+      { value: 'juice', label: 'Juice', trigger: '6' },
+      { value: 'No', label: 'No', trigger: '8' },
+    ],
+  },
+
+  {
+    id: '6',
+    user: true,
+    validator: (value) => {
+      if (isNaN(value)) {
+        return 'Please enter quantity';
+      } else if (value < 1) {
+        return 'Please enter a valid quantity';
+      } else {
+        return true;
+      }
+    },
+    trigger: '8',
+  },
+  {
+    id: '8',
+    message: 'Great! Your order has been placed. Would you like to order anything else?',
+    trigger: '9',
+  },
+  {
+    id: '9',
+    options: [
+      { value: 'yes', label: 'Yes', trigger: '3' },
+      { value: 'no', label: 'No', trigger: '10' },
+    ],
+  },
+  {
+    id: '10',
+    message: 'Thank you for your order, {previousValue}! We will prepare it right away.',
+    end: true,
+  },
+];
+const config = {
+  floating: true,
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      conversationHistory: [],
+    };
+  }
+
+  handleEnd = ({ steps, values }) => {
+    const conversationHistory = [...this.state.conversationHistory];
+    conversationHistory.push({
+      type: 'user',
+      message: values.name,
+    });
+    steps.forEach((step) => {
+      if (step.message) {
+        conversationHistory.push({
+          type: 'bot',
+          message: step.message,
+        });
+      }
+    });
+    this.setState({ conversationHistory });
+  }
+
+  render() {
+    const { conversationHistory } = this.state;
+    return (
+      <div className="App">
+        <ThemeProvider theme={theme}>
+          <ChatBot
+            headerTitle="Nosh-Bot"
+            
+            steps={steps}
+            {...config}
+            botAvatar={botAvatar}
+            headerAvatar={botAvatar}
+            handleEnd={this.handleEnd}
+          />
+          <div className="conversation-container">
+            {conversationHistory.map((message, index) => {
+              if (message.type === 'user') {
+                return (
+                  <div key={index} className="user-message">
+                    <span>{message.message}</span>
+                  </div>
+                );
+              } else {
+                return (
+                  <div key={index} className="bot-message">
+                    <span>{message.message}</span>
+                  </div>
+                );
+              }
+            })}
+          </div>
+        </ThemeProvider>
+      </div>
+    );
+  }
 }
 
 export default App;
